af6515779afdcc9969a3da9593ccae850fe0443e
Parag Dewangan <118594250+Agent1236@users.noreply.github.com>
2024-05-11 22:19:18 +0530

    Update CloudSpeechClient.h

--- Speech_To_Text_ESP32/CloudSpeechClient.h
+++ Speech_To_Text_ESP32/CloudSpeechClient.h
@@ -5,19 +5,19 @@
 #include "Audio.h"
 
 enum Authentication {
-  USE_ACCESSTOKEN,
-  USE_APIKEY
+    USE_ACCESSTOKEN,
+    USE_APIKEY
 };
 
 class CloudSpeechClient {
-  WiFiClientSecure client;
-  void PrintHttpBody2(Audio* audio);
-  Authentication authentication;
-
-public:
-  CloudSpeechClient(Authentication authentication);
-  ~CloudSpeechClient();
-  void Transcribe(Audio* audio);
+    WiFiClientSecure client;
+    void PrintHttpBody2(Audio* audio);
+    Authentication authentication;
+  
+    public:
+        CloudSpeechClient(Authentication authentication);
+        ~CloudSpeechClient();
+        void Transcribe(Audio* audio);
 };
 
-#endif // _CLOUDSPEECHCLIENT_H
+#endif
